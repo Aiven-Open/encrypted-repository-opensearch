d5ff72d32472af96d0f32edbeb25e1b917a559cd
Andrey Pleskach <ples@aiven.io>
2022-01-28 22:50:17 +0100

    Miscs changes
    - Removed default classes
    - Added checkstyle
    - Renamed plugin from the default name to encrypted-repository
    - Added empty EncryptedRepositoryPlugin class
    
    Signed-off-by: Andrey Pleskach <ples@aiven.io>

---
+++ src/main/java/org/opensearch/repository/encrypted/EncryptedRepositoryPlugin.java
@@ -0,0 +1,12 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.opensearch.repository.encrypted;
+
+import org.opensearch.plugins.Plugin;
+import org.opensearch.plugins.RepositoryPlugin;
+
+public class EncryptedRepositoryPlugin extends Plugin implements RepositoryPlugin {
+}
